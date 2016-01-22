87e83b3fd25b351182f4f4a22223080b4f97148a
Glen Mailer <glenjamin@gmail.com>
2016-01-22 00:29:30 +0000

    Routing API tweaks and more tests

--- test/routes.js
+++ test/routes.js
@@ -2,25 +2,58 @@
 var expect = require('chai').expect;
 
 var routes = require('../lib/routes');
-var createHref = routes.createHref;
+var href = routes.href;
+var path = routes.path;
 
-describe('createHref()', function() {
+describe('routes', function() {
 
-  it('accepts createHref()', function() {
-    expect(createHref()).to.eql('/#/');
+  function route(pathname) {
+    var match;
+    routes.createRouter(function(_match) {
+      match = _match;
+    })(pathname);
+    return match;
+  }
+
+  describe('path & href()', function() {
+    it('accepts no args', function() {
+      expect(href()).to.eql('/#/');
+      expect(route(path())).to.eql({});
+    });
+
+    it('accepts (namespace)', function() {
+      expect(href('buttons')).to.eql('/#/buttons');
+      expect(route(path('buttons'))).to.eql({ namespace: 'buttons' });
+    });
+
+    it('accepts (namespace, card)', function() {
+      expect(href('buttons', 'loading')).to.eql('/#/buttons/loading');
+      expect(route(path('buttons', 'loading'))).to.eql({
+         namespace: 'buttons', card: 'loading'
+      });
+    });
+
+    it('should handle URI components', function() {
+      expect(href('Fancy buttons', 'Loading button')).to.eql(
+        '/#/Fancy%20buttons/Loading%20button'
+      );
+      expect(route(path('Fancy buttons', 'Loading button'))).to.eql({
+         namespace: 'Fancy buttons', card: 'Loading button'
+      });
+    });
   });
-
-  it('accepts createHref(namespace)', function() {
-    expect(createHref('buttons')).to.eql('/#/buttons');
-  });
-
-  it('accepts createHref(namespace, card)', function() {
-    expect(createHref('buttons', 'loading')).to.eql('/#/buttons/loading');
-  });
-
-  it('should handle URI components', function() {
-    expect(createHref('Fancy buttons', 'Loading button')).to.eql(
-      '/#/Fancy%20buttons/Loading%20button'
-    );
+  describe("routing", function() {
+    it('should route / to {}', function() {
+      expect(route('/')).to.eql({});
+    });
+    it('should route /abc to { namespace: abc }', function() {
+      expect(route('/abc')).to.eql({ namespace: 'abc' });
+    });
+    it('should route /abc/123 to { namespace: abc, card: 123 }', function() {
+      expect(route('/abc/123')).to.eql({ namespace: 'abc', card: '123' });
+    });
+    it('should route /not/a/thing to null', function() {
+      expect(route('/not/a/thing')).to.eql(null);
+    });
   });
 });
